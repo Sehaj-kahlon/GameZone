5a6f3d90ec133027ff763df3c062309926f9c7ba
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-06-11 02:41:23 +0530

    Play now button styled

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-document.addEventListener(
-  "webkitfullscreenchange",
-  function () {
-    var vid = document.getElementById("background-video");
-    if (document.webkitIsFullScreen == true) {
-      vid.play();
-    } else {
-      vid.pause();
-    }
-  },
-  false
-);
+function modulo(number, mod) {
+  let result = number % mod;
+  if (result < 0) {
+    result += mod;
+  }
+  return result;
+}
 
-document.addEventListener(
-  "fullscreenchange",
-  function () {
-    console.log(document.fullscreen);
-    var vid = document.getElementById("background-video");
-    if (document.fullscreen == true) {
-      vid.play();
-    } else {
-      vid.pause();
-    }
-  },
-  false
-);
-document.addEventListener(
-  "mozfullscreenchange",
-  function () {
-    console.log(document.mozFullScreen);
-    var vid = document.getElementById("background-video");
-    if (document.mozFullScreen == true) {
-      vid.play();
-    } else {
-      vid.pause();
-    }
-  },
-  false
-);
+function setUpCarousel(carousel) {
+  function handleNext() {
+    currentSlide = modulo(currentSlide + 1, numSlides);
+    changeSlide(currentSlide);
+  }
+
+  function handlePrevious() {
+    currentSlide = modulo(currentSlide - 1, numSlides);
+    changeSlide(currentSlide);
+  }
+
+  function changeSlide(slideNumber) {
+    carousel.style.setProperty("--current-slide", slideNumber);
+  }
+
+  // get elements
+  const buttonPrevious = carousel.querySelector("[data-carousel-button-previous]");
+  const buttonNext = carousel.querySelector("[data-carousel-button-next]");
+  const slidesContainer = carousel.querySelector("[data-carousel-slides-container]");
+
+  // carousel state we need to remember
+  let currentSlide = 0;
+  const numSlides = slidesContainer.children.length;
+
+  // set up events
+  buttonPrevious.addEventListener("click", handlePrevious);
+  buttonNext.addEventListener("click", handleNext);
+}
+
+const carousels = document.querySelectorAll("[data-carousel]");
+carousels.forEach(setUpCarousel);
