793afc9e70c8fd494358991f32cc0d7d1bf92298
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-07-01 17:25:19 +0530

    remove hamburger on scroll

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ if (typeof document !== "undefined") {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
   });
+  window.onscroll = function () {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  };
   document.querySelectorAll(".nav-link").forEach((n) =>
     n.addEventListener("click", () => {
       hamburger.classList.remove("active");
